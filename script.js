64fbe0d67af06cf20c00cb74d1021f95cc938cdf
Ricardo Filipe Marinho Braz <47540686+RicardoMarinho@users.noreply.github.com>
2019-04-05 21:26:05 +0100

    remove dentro do adicionar

--- script.js
+++ script.js
@@ -14,21 +14,20 @@ form.addEventListener('submit', function(event) {
     line.innerHTML = `<tr><td>${description}</td><td><input value=${quantity}></td><td><input type='button' value='Remove'></tr>`;
     let table = document.getElementById('products');
     table.append(line);
+
+    let remove = line.querySelector('input[type=button');
+
+    remove.addEventListener('click', function(){
+        this.parentNode.parentNode.remove();
+        updateTotal();
+    });
+
+
     document.querySelector('form input[name=description').value="";
     document.querySelector('form input[name=quantity').value="";
     updateTotal();
     event.preventDefault();
 })
-
-
-let remove = line.querySelector('input[type=button');
-
-remove.addEventListener('click', function(){
-    this.parentNode.parentNode.remove();
-    updateTotal();
-});
-
-
 
 function updateTotal(){
     let total = 0;
