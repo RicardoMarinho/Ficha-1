cc11d8e0458d1bfad3229c4f706fbc8704d34c48
Ricardo Filipe Marinho Braz <47540686+RicardoMarinho@users.noreply.github.com>
2019-04-05 21:11:26 +0100

    Ficha 3

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+window.addEventListener('load', function(){
+    let products = document.getElementById('products');
+});
+
+let form = document.getElementsByTagName('form')[0];
+form.addEventListener('submit', function(event) {
+    let line = document.createElement('tr');
+
+    let description = document.querySelector('form input[name=description]').value;
+    let quantity = document.querySelector('form input[name=quantity]').value;
+
+    line.innerHTML = `<tr><td>${description}</td><td><input value=${quantity}></td><td><input type='button' value='Remove'></tr>`;
+    let table = document.getElementById('products');
+    table.append(line);
+
+    event.preventDefault();
+})
+
+
+let remove = line.querySelector('input[type=button');
+
+remove.addEventListener('click', function(){
+    this.parentNode.parentNode.remove();
+});
+
+document.querySelector('form input[name=description').value="";
+document.querySelector('form input[name=quantity').value="";
