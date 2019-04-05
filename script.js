886b6610dfe679aa97d1ea45b3739cdf315b6b3f
Ricardo Filipe Marinho Braz <47540686+RicardoMarinho@users.noreply.github.com>
2019-04-05 21:20:35 +0100

    updateTotal

--- script.js
+++ script.js
@@ -14,7 +14,9 @@ form.addEventListener('submit', function(event) {
     line.innerHTML = `<tr><td>${description}</td><td><input value=${quantity}></td><td><input type='button' value='Remove'></tr>`;
     let table = document.getElementById('products');
     table.append(line);
-
+    document.querySelector('form input[name=description').value="";
+    document.querySelector('form input[name=quantity').value="";
+    updateTotal();
     event.preventDefault();
 })
 
@@ -23,7 +25,19 @@ let remove = line.querySelector('input[type=button');
 
 remove.addEventListener('click', function(){
     this.parentNode.parentNode.remove();
+    updateTotal();
 });
 
-document.querySelector('form input[name=description').value="";
-document.querySelector('form input[name=quantity').value="";
+
+
+function updateTotal(){
+    let total = 0;
+    let lines = document.querySelectorAll('table tr');
+    for(let i = 0; i < lines.length; i++){
+        console.log(lines[i]);
+        let input = lines[i].getElementsByTagName('input')[0];
+        if(input != null)   total+=Number(input.value);
+    }
+    let span = document.getElementById('total');
+    span.innerHTML = total;
+}
